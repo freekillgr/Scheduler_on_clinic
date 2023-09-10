7d0c27c8216cbbce7c0a2dbc548969f6be34cd10
Panagiotis Patronidis <82458495+freekillgr@users.noreply.github.com>
2023-09-10 20:52:58 +0300

    Update Schedule_calculator.py
    fixes

--- Schedule_calculator.py
+++ Schedule_calculator.py
@@ -2,33 +2,37 @@ import os
 import pandas as pd
 from datetime import datetime, timedelta
 
-# Function to read the CSV file
 def read_csv_file(file_path):
     try:
         return pd.read_csv(file_path)
     except FileNotFoundError:
         print(f"Error: {file_path} not found.")
         exit(1)
-    except pd.errors.EmptyDataError:
-        print(f"Error: {file_path} is empty.")
-        exit(1)
-    except pd.errors.ParserError:
-        print(f"Error: Could not parse {file_path}.")
-        exit(1)
 
-# Initialize file paths and variables
+# Define the schedule file path
 schedule_file_path = 'clinic_schedule.csv'
+
+# Check if the schedule file already exists and delete it if it does
+if os.path.exists(schedule_file_path):
+    os.remove(schedule_file_path)
+
+# Read the CSV files
 data = read_csv_file('doctor_data.csv')
 wishes = read_csv_file('doctor_wishes.csv')
+
+# Define the start and end date for scheduling
 start_date = datetime.strptime('2023-09-01', '%Y-%m-%d').date()
 end_date = datetime.strptime('2023-09-30', '%Y-%m-%d').date()
 delta = end_date - start_date
 num_days = delta.days + 1
+
+# Initialize variables
 doctors = data['Doctor_Name'].tolist()
 max_monthly_calls = num_days // len(doctors)
 doctor_counters = {doctor: 0 for doctor in doctors}
 monthly_counters = {doctor: 0 for doctor in doctors}
-weekend_counters = {doctor: 0 for doctor in doctors}  # Weekend counter added
+weekend_counters = {doctor: 0 for doctor in doctors}
+last_weekend_worked = {doctor: None for doctor in doctors}
 schedule = {}
 previous_day_doctors = []
 current_date = start_date
@@ -36,7 +40,7 @@ round_robin_index_open = 0
 round_robin_index_closed = 0
 call_type = 1  # Initialize call type (0 for open, 1 for closed)
 
-# Convert wishes to a dictionary
+# Convert wishes to a dictionary of datetime objects
 doctor_wishes_dict = {}
 for index, row in wishes.iterrows():
     doc = row['Doctor_Name']
@@ -48,35 +52,25 @@ for index, row in wishes.iterrows():
 
 # Generate the schedule
 for day in range(num_days):
-    is_weekend = current_date.weekday() >= 5  # 5 = Saturday, 6 = Sunday
+    is_weekend = current_date.weekday() >= 5  # True if it's Saturday or Sunday
+
     num_doctors_needed = 3 if call_type == 0 else 1
 
-    # Weekend considerations integrated into available_doctors
-    if is_weekend:
-        available_doctors_df = data[
-            (~data['Doctor_Name'].isin(previous_day_doctors)) &
-            (~data['Doctor_Name'].isin(doctor_wishes_dict.get(current_date, [])))
-        ]
-        available_doctors_df['Weekend_Count'] = available_doctors_df['Doctor_Name'].map(weekend_counters)
-        available_doctors_df = available_doctors_df.sort_values('Weekend_Count')
-        available_doctors = available_doctors_df['Doctor_Name'].tolist()
+    # Determine available doctors based on call type and other conditions
+    if call_type == 0:
+        available_doctors_df = data[(~data['Doctor_Name'].isin(previous_day_doctors)) & (~data['Doctor_Name'].isin(doctor_wishes_dict.get(current_date, [])))]
     else:
-        if call_type == 0:
-            available_doctors_df = data[
-                (~data['Doctor_Name'].isin(previous_day_doctors)) &
-                (~data['Doctor_Name'].isin(doctor_wishes_dict.get(current_date, [])))
-            ]
-        else:
-            available_doctors_df = data[
-                (data['Status'] == 'n') &
-                (~data['Doctor_Name'].isin(previous_day_doctors)) &
-                (~data['Doctor_Name'].isin(doctor_wishes_dict.get(current_date, [])))
-            ]
-        available_doctors = available_doctors_df['Doctor_Name'].tolist()
+        available_doctors_df = data[(data['Status'] == 'n') & (~data['Doctor_Name'].isin(previous_day_doctors)) & (~data['Doctor_Name'].isin(doctor_wishes_dict.get(current_date, [])))]
+
+    available_doctors = available_doctors_df['Doctor_Name'].tolist()
+
+    # Filter out doctors who worked the last weekend, if it's a weekend
+    if is_weekend:
+        available_doctors = [doc for doc in available_doctors if last_weekend_worked[doc] != current_date - timedelta(days=7)]
 
     if len(available_doctors) >= num_doctors_needed:
         selected_doctors = []
-
+        
         if call_type == 0:
             for _ in range(num_doctors_needed):
                 if round_robin_index_open >= len(available_doctors):
@@ -95,41 +89,45 @@ for day in range(num_days):
         for selected_doctor in selected_doctors:
             doctor_counters[selected_doctor] = 1
             monthly_counters[selected_doctor] += 1
-            if is_weekend:
-                weekend_counters[selected_doctor] += 1  # Increment weekend counter if it's a weekend
             if monthly_counters[selected_doctor] >= max_monthly_calls:
                 available_doctors.remove(selected_doctor)
+
+        # Update last_weekend_worked and weekend_counters if it's a weekend
+        if is_weekend:
+            for selected_doctor in selected_doctors:
+                last_weekend_worked[selected_doctor] = current_date
+                weekend_counters[selected_doctor] += 1
                 
         schedule[current_date] = selected_doctors
-
+    
     else:
-        least_assigned_doctors = sorted(doctors, key=lambda x: (monthly_counters[x], weekend_counters[x]))[:num_doctors_needed]
+        least_assigned_doctors = sorted(doctors, key=lambda x: monthly_counters[x])[:num_doctors_needed]
         selected_doctors = least_assigned_doctors
-
         for selected_doctor in selected_doctors:
             doctor_counters[selected_doctor] = 1
             monthly_counters[selected_doctor] += 1
-            if is_weekend:
-                weekend_counters[selected_doctor] += 1  # Increment weekend counter if it's a weekend
-
         schedule[current_date] = selected_doctors
 
+    # Update the list of doctors who worked on the previous day's call
     previous_day_doctors = selected_doctors
+
+    # Move to the next day and toggle call type
     current_date += timedelta(days=1)
     call_type = 1 - call_type
 
-# Save the schedule to a CSV file
+# Save the schedule and monthly counters to CSV files
 schedule_df = pd.DataFrame(list(schedule.items()), columns=['Date', 'Doctors'])
 schedule_df.to_csv(schedule_file_path, index=False)
 
-# Display the schedule
+monthly_counters_df = pd.DataFrame(list(monthly_counters.items()), columns=['Doctor_Name', 'Days_Worked'])
+monthly_counters_df.to_csv('monthly_counters.csv', index=False)
+
+# Save weekend counters to a CSV
+weekend_counters_df = pd.DataFrame(list(weekend_counters.items()), columns=['Doctor_Name', 'Weekends_Worked'])
+weekend_counters_df.to_csv('weekend_counters.csv', index=False)
+
+# Display the schedule, monthly counters, and weekend counters
 for date, doctors in schedule.items():
     print(f"{date}: {', '.join(doctors)}")
 
-print("Monthly counters for each doctor:")
-for doctor, count in monthly_counters.items():
-    print(f"{doctor}: {count} days")
-
-print("Weekend counters for each doctor:")
-for doctor, count in weekend_counters.items():
-    print(f"{doctor}: {count} weekends")
+print("Monthly counter
